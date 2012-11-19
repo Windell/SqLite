73f1d4b8eda419df492f9d78cc432a5d51b1f52c
Guo Tingfeng <guotingfeng@hisense.com>
2012-11-19 17:37:45 +0800

    Adding content provider test code and configuration adjust

--- src/com/jamdeo/sqlite/test/TestContentProvider.java
+++ src/com/jamdeo/sqlite/test/TestContentProvider.java
@@ -3,19 +3,35 @@ package com.jamdeo.sqlite.test;
 import android.database.Cursor;
 import android.net.Uri;
 import android.test.AndroidTestCase;
+import android.util.Log;
 
-import com.jamdeo.sqlite.MyContentProvider;
+import com.jamdeo.sqlite.ProgramContentProvider;
 import com.jamdeo.sqlite.pojo.Category;
 
 public class TestContentProvider extends AndroidTestCase {
 	public void testCategoryQuery() {
-		Cursor cursor=getContext().getContentResolver().query(
-				Uri.parse("content://" + MyContentProvider.AUTHRORITY + "/"
-						+ MyContentProvider.TABLE_CATEGORY),
+		Cursor cursor = getContext().getContentResolver().query(
+				Uri.parse("content://" + ProgramContentProvider.AUTHRORITY
+						+ "/" + ProgramContentProvider.TABLE_CATEGORY),
 				new String[] { "_id", "categoryid", "name", "description" },
 				null, null, null);
-		while(cursor.moveToNext()){
-			new Category(cursor.getLong(1),cursor.getString(2),cursor.getString(3)).toString();
+		while (cursor.moveToNext()) {
+			Log.i("Category",
+					new Category(cursor.getLong(1), cursor.getString(2), cursor
+							.getString(3)).toString());
+		}
+	}
+
+	public void testSingleCategoryQuery() {
+		Cursor cursor = getContext().getContentResolver()
+				.query(Uri.parse("content://"
+						+ ProgramContentProvider.AUTHRORITY + "/"
+						+ ProgramContentProvider.TABLE_CATEGORY + "/" + 3),
+						null, null, null, null);
+		while (cursor.moveToNext()) {
+			Log.i("SingleCategory",
+					new Category(cursor.getLong(1), cursor.getString(2), cursor
+							.getString(3)).toString());
 		}
 	}
 }
---
+++ src/com/jamdeo/sqlite/test/TestSQLiteHelper.java
@@ -0,0 +1,48 @@
+package com.jamdeo.sqlite.test;
+
+import java.util.List;
+
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+import com.jamdeo.sqlite.DataBaseManager;
+import com.jamdeo.sqlite.pojo.Category;
+
+public class TestSQLiteHelper extends AndroidTestCase {
+	private static final String TAG = "TestCategory";
+
+	public void testSaveCategory() {
+		DataBaseManager dbm = new DataBaseManager(this.getContext());
+		dbm.insert(new Category(1L, "Movies", "This is the first test case"));
+		dbm.insert(new Category(2L, "Series", "Series like ege of ice and fire"));
+		Log.i(TAG, "Fisrt Category Added");
+	}
+
+	public void testQueryCategory() {
+		DataBaseManager dbm = new DataBaseManager(getContext());
+		List<Category> categorys = dbm.query("select * from category");
+		for (Category category : categorys) {
+			Log.i("TestQuery", category.toString());
+		}
+	}
+
+	public void testFindCategory() {
+		DataBaseManager dbm = new DataBaseManager(getContext());
+		Category category = (Category) dbm.findById(2L,Category.class);
+		Log.i("TestFind", category == null ? "" : category.toString());
+	}
+
+	public void testUpdateCategory() {
+		DataBaseManager dbm = new DataBaseManager(getContext());
+		dbm.update(new Category(2L, "Sitcom",
+				"Song of ice and fire, clash of the kings"));
+		testFindCategory();
+	}
+
+//	public void testDeleteCategory() {
+//		DataBaseManager dbm = new DataBaseManager(getContext());
+//		dbm.delete(1L,Category.class);
+//		Log.i("TestDelete", "Delete finished.....");
+//		testQueryCategory();
+//	}
+}
